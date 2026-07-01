5d86ae11be9dfa916e282e26450f7dbe17b7ba49
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-07-01 16:09:29 +0600

    LAB-01 || Task-03

--- Player.java
+++ Player.java
@@ -3,3 +3,24 @@ public class Player {
     public int jersey_number;
     public String position;
 }
+
+//Tester:
+// public class PlayerTester{
+//     public static void main(String args[]){
+//         Player player1 = new Player();
+//         player1.name = "Messi";
+//         player1.jersey_number = 10;
+//         player1.position = "Forward";
+//         System.out.println("Name of the Player: "+ player1.name);
+//         System.out.println("Jersey Number of player: "+ player1.jersey_number);
+//         System.out.println("Position of player: "+ player1.position);
+//         System.out.println("=====================");
+//         Player player2 = new Player();
+//         player2.name = "Neuer";
+//         player2.jersey_number = 1;
+//         player2.position = "Goal Keeper";
+//         System.out.println("Name of the player: "+ player2.name);
+//         System.out.println("Jersey Number of player: "+ player2.jersey_number);
+//         System.out.println("Position of player: "+ player2.position);
+//     }
+// }
