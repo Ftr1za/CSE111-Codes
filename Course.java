858ab9136681ba4befdac70c21caac9a75a53e4c
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-07-01 16:00:09 +0600

    LAB-02 || Task-02

--- Course.java
+++ Course.java
@@ -14,3 +14,20 @@ public class Course {
         System.out.println("Course Credit: "+this.credit);
     }
 }
+
+//Tester:
+// public class Tester2 {
+//     public static void main(String[] args) {
+//         Course c1 = new Course();
+//         Course c2 = new Course();
+//         c1.updateDetails("Programming Language I","CSE110", 3);
+//         System.out.println("========== 1 ==========");
+//         c1.displayCourse();
+//         c2.updateDetails("Data Structures","CSE220", 3);
+//         System.out.println("========== 2 ==========");
+//         c2.displayCourse();
+//         c1.updateDetails("Programming Language II","CSE111", 3);
+//         System.out.println("========== 3 ==========");
+//         c1.displayCourse();
+//     }
+// }
