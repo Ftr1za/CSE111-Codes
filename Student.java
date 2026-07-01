12418d42764e586c3258d71407d2acd8526a2aaa
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-07-01 16:10:45 +0600

    LAB-01 || Task-04

--- Student.java
+++ Student.java
@@ -2,3 +2,16 @@ public class Student {
     public String name = "Default";
     public int id;
 }
+
+//Tester:
+// public class StudentTester {
+//     public static void main(String [] args){
+//     Student s1 = new Student();
+//     System.out.println("Name of the Student: "+s1.name);
+//     System.out.println("ID of the Student: "+s1.id);
+//     s1.name = "Bob";
+//     s1.id = 123;
+//     System.out.println("Name of the Student: "+s1.name);
+//     System.out.println("ID of the Student: "+s1.id);
+//     }
+// }
